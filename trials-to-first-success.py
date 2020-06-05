5b3718e8dc8fd4b3a16eddd233dd8fac9f6fda27
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2020-06-05 13:44:52 -0500

    refactor(trials-to-first-success): move counting calls to random() to a different function, allowing me to use a list comprehension in main().

--- trials-to-first-success.py
+++ trials-to-first-success.py
@@ -7,15 +7,16 @@ import statistics
 # change this to test different probabilities
 chance_of_success = 0.1
 
-def main():
-    attempts = []
-    for _ in range(1000):
-        # keep rolling until we succeed
-        count = 1
-        while random.random() > chance_of_success:
-            count += 1
-        attempts.append(count)
+# returns number of attempts
+def roll_until_success():
+    count = 1
+    while random.random() > chance_of_success:
+        count += 1
+    return count
 
+def main():
+    num_attempts = 10000
+    attempts = [roll_until_success() for _ in range(num_attempts)]
     print("expected attempts: " + str(1.0/chance_of_success))
     print("average attempts: " + str(statistics.mean(attempts)))
 
